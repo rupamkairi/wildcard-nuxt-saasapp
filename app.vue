3d45af2148d4547d2f44ca26bd0d36d37da8d1cb
Rupam Kairi <rupamkairi@hotmail.com>
2023-03-26 03:05:13 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtWelcome />
+
+    <code>{{ subdomain }}</code>
+  </div>
+</template>
+
+<script setup>
+const ssr = useNuxtApp()?.ssrContext;
+const host = ssr?.event?.node?.req?.headers?.host;
+const subdomain = host?.split(".")?.[0];
+// console.log(subdomain);
+</script>
